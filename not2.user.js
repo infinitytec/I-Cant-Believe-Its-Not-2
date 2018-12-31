b04439fcbd98894ad98d349424217eff8ea4c558
infinitytec <infinitytec@users.noreply.github.com>
2018-12-31 16:20:26 -0600

    fixes and changes
    -better sizing
    -blocks no longer recolored due to annoying bugs (I will try to recolor them later)

--- not2.user.js
+++ not2.user.js
@@ -11,28 +11,28 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    //From NickyNouse
-    // inject 2.0 stylesheet
-    var style = document.createElement('link');
-    style.setAttribute('rel', 'stylesheet');
-    style.setAttribute('href', 'https://mrjacobbloom.github.io/scratch-themes-resources/2/style.css');
-    document.head.appendChild(style);
+   'use strict';
+//    //Adapeted from NickyNouse
+//    // inject 2.0 stylesheet
+//    var style = document.createElement('link');
+//    style.setAttribute('rel', 'stylesheet');
+//    style.setAttribute('href', 'https://mrjacobbloom.github.io/scratch-themes-resources/2/style.css');
+//    document.head.appendChild(style);
    // import @comp09's filters from tjvr/scratchblocks
-    fetch('https://mrjacobbloom.github.io/scratch-themes-resources/2/filters.svg').then(function(response) {
-        return response.text();
-    }).then(function(response) {
-        var doc = new DOMParser().parseFromString(response, 'image/svg+xml');
-        var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        svg.appendChild(document.importNode(doc.documentElement.firstElementChild, true));
-        document.body.appendChild(svg);
-    });
+//    fetch('https://mrjacobbloom.github.io/scratch-themes-resources/2/filters.svg').then(function(response) {
+//        return response.text();
+//    }).then(function(response) {
+//        var doc = new DOMParser().parseFromString(response, 'image/svg+xml');
+//        var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+//        svg.appendChild(document.importNode(doc.documentElement.firstElementChild, true));
+//        document.body.appendChild(svg);
+//    });
     //Reposition everything
     GM_addStyle("html {overflow-y: hidden; min-width: 1240px;}");
     GM_addStyle(".gui_body-wrapper_-N0sA {height: calc(100% - 2rem);}");
     GM_addStyle(".gui_flex-wrapper_uXHkj{}");
-    GM_addStyle(".gui_editor-wrapper_2DYcj {position: absolute; width: calc(100% - 500px); right:0; height: 95%;}");
-    GM_addStyle(".gui_stage-and-target-wrapper_69KBf {position: absolute; width: 40%; left: 0; height: 95%;}");
+    GM_addStyle(".gui_editor-wrapper_2DYcj {position: absolute; width: calc(100% - 500px); right:0; height: 97%;}");
+    GM_addStyle(".gui_stage-and-target-wrapper_69KBf {position: absolute; width: 40%; left: 0; height: 97%;}");
     GM_addStyle(".blocklyText {font-size: 10pt;}");
     GM_addStyle(".gui_stage-menu-wrapper_W5Ler {border-radius: 10px 10px 0px 0px;border: 1px solid #D0D1D2;margin-left: 0.5rem;margin-right: 0.5rem;margin-top: 5px;}");
     GM_addStyle(".stage-selector_header_2GVr1, .gui_stage-menu-wrapper_W5Ler, .sprite-info_sprite-info_3EyZh, .gui_tab_27Unf.gui_is-selected_sHAiu {border: 1px solid #D0D1D2;background: #fefefe; /* Old browsers */background: -moz-linear-gradient(top, #fefefe 0%, #e7e9e9 100%); /* FF3.6-15 */background: -webkit-linear-gradient(top, #fefefe 0%,#e7e9e9 100%); /* Chrome10-25,Safari5.1-6 */background: linear-gradient(to bottom, #fefefe 0%,#e7e9e9 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fefefe', endColorstr='#e7e9e9',GradientType=0 ); /* IE6-9 */}");
@@ -44,7 +44,7 @@
     GM_addStyle(".controls_controls-container_2xinB {position: relative; left: 87%;");
     GM_addStyle(" .gui_tab_27Unf.gui_is-selected_sHAiu { color: #5C5D5F;} .stage-header_stage-menu-wrapper_15JJt box_box_2jjDp{background: -moz-linear-gradient(top, #fefefe 0%, #e7e9e9 100%); }");
     GM_addStyle(".menu-bar_menu-bar_JcuHF {height: 2rem; background-color: #9C9EA2;}");
-    GM_addStyle(".menu-bar_menu-item_2lssx {padding: 0 .5rem;line-height: 2rem;}");
+    GM_addStyle(".menu-bar_menu-item_2lssx {padding: 0 .5rem;line-height: 2rem;}.selector_wrapper_8_BHs, .selector_wrapper_8_BHs {background: #e6e8e8;} span.button_outlined-button_1bS__:nth-child(2){position: absolute;}");
     GM_addStyle(".menu-bar_scratch-logo_2uReV {height: 2rem; padding: 0.3rem 0;}");
     GM_addStyle(".language-selector_language-select_8Vfnm {height: 1.5rem; background: rgba(255, 255, 255, 0.1); border: 1px solid #9C9EA2; color: white;}");
     GM_addStyle(".icon-button_container_278u5 {color: #9C9EA2;} .stage_stage_1fD7k{border-radius: 0;}");
