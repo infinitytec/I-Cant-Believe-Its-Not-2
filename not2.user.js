a80b88b42e1832d75918826cb606ff696a4f2fcd
infinitytec <infinitytec@users.noreply.github.com>
2019-01-06 20:39:28 -0600

    Update not2.user.js

--- not2.user.js
+++ not2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         I can't believe it's not 2
 // @namespace    infinitytec.github.io
-// @version      1.5.2
+// @version      1.6
 // @description  Making 3.0 look like 2.
 // @author       infinitytec
 // @match        https://llk.github.io/scratch-gui/*
