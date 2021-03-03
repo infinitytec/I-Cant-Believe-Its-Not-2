8ecf5ecbcbcb1b9ca1145661c9b4295d3251417a
infinitytec <infinitytec@users.noreply.github.com>
2021-03-02 20:12:29 -0600

    should fix display issues

--- not2.user.js
+++ not2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         I can't believe it's not 2
 // @namespace    infinitytec.github.io
-// @version      1.8.2
+// @version      1.8.3
 // @description  Making Scratch 3.0 look like Scratch 2.
 // @author       infinitytec
 // @match        https://llk.github.io/scratch-gui/*
@@ -22,7 +22,7 @@
     GM_addStyle(".gui_body-wrapper_-N0sA {height: calc(100% - 2rem);} .stage-header_stage-size-row_14N65 {position:absolute;}");
     GM_addStyle(".gui_flex-wrapper_uXHkj{} .stage_stage_1fD7k {border: none;}");
     GM_addStyle(".gui_editor-wrapper_2DYcj {position: absolute; width: calc(100% - 500px); right:0; height: 97%;}");
-    GM_addStyle(".gui_stage-and-target-wrapper_69KBf {position: absolute; width: 40%; left: 0; height: 97%;}");
+    GM_addStyle(".gui_stage-and-target-wrapper_69KBf {position: absolute; left: 0; height: 97%;}");
     GM_addStyle(".blocklyText {font-size: 10pt;} .sprite-selector_sprite-selector_2KgCX {width: 428px; } .menu_menu_3k7QT {background: #9c9ea2; top: 30px;}");
     GM_addStyle(".gui_stage-menu-wrapper_W5Ler {border-radius: 10px 10px 0px 0px;border: 1px solid #D0D1D2;margin-left: 0.5rem;margin-right: 0.5rem;margin-top: 5px;}");
     GM_addStyle(".stage-selector_header_2GVr1, .gui_stage-menu-wrapper_W5Ler, .sprite-info_sprite-info_3EyZh, .gui_tab_27Unf.gui_is-selected_sHAiu {border: 1px solid #D0D1D2;background: #fefefe; /* Old browsers */background: -moz-linear-gradient(top, #fefefe 0%, #e7e9e9 100%); /* FF3.6-15 */background: -webkit-linear-gradient(top, #fefefe 0%,#e7e9e9 100%); /* Chrome10-25,Safari5.1-6 */background: linear-gradient(to bottom, #fefefe 0%,#e7e9e9 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fefefe', endColorstr='#e7e9e9',GradientType=0 ); /* IE6-9 */}");
@@ -42,4 +42,5 @@
     GM_addStyle(".icon-button_container_278u5:hover {color: #007BE7 !important; -webkit-transform: scale(1.0);-ms-transform: scale(1.0);transform: scale(1.0);filter: saturate(100%);}");
     GM_addStyle(".monitor_monitor_11Vhm {background: #B6B6B6; border: 1.5px solid #9C9EA2;} .monitor_value_3Yexa { box-shadow: inset .5px .5px 2px #4d4d4d; border: 1px solid white;} .menu-bar_menu-bar-item_oLDa-{height: 2rem;}");
     GM_addStyle("div.menu-bar_menu-bar-item_oLDa-:nth-child(5){position: relative;top: 3rem;right: 14rem;}input.input_input-form_l9eYg:nth-child(1){border: 1px solid #D0D1D2;background: white;color: black;} .gui, .index_app_3Qs6X{overflow: hidden;} .stage-selector_costume-canvas_2L_6h {background: white;} .stage-selector_header-title_33xCt{color: hsla(225, 15%, 40%, 1);}.stage-wrapper_stage-wrapper_2bejr{border-radius: 5px 5px 0 0;background: linear-gradient(to bottom, #fefefe 0%,#e7e9e9 10%);border: 1px solid #D0D1D2;margin: 10px 0 0 auto;}.gui_body-wrapper_-N0sA {background: white;} .backpack_backpack-header_6ltCS{border-radius: 5px 5px 0 0;background: linear-gradient(to bottom, #fefefe 0%,#e7e9e9 100%)}");
+    GM_addStyle(".stage-wrapper_stage-wrapper_2bejr.stage-wrapper_full-screen_2hjMb {margin-top: 0}");
 })();
